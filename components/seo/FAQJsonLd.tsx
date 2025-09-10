6533e9c78f24b6c94e1d0d6c547ac9eade707d5a
Turbo-89 <verloo.wim@icloud.com>
2025-09-10 09:00:55 +0200

    Create FAQJsonLd.tsx

---
+++ components/seo/FAQJsonLd.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export type FAQItem = { question: string; answer: string };
+
+export default function FAQJsonLd({ items }: { items: FAQItem[] }) {
+  const data = {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    mainEntity: items.map((it) => ({
+      "@type": "Question",
+      name: it.question,
+      acceptedAnswer: { "@type": "Answer", text: it.answer },
+    })),
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
