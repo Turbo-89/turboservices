13129c48be060fc38c8bd20d8fd692be6a1165da
Turbo-89 <verloo.wim@icloud.com>
2025-11-18 12:08:24 +0100

    Update slugify.ts

--- lib/slugify.ts
+++ lib/slugify.ts
@@ -1,6 +1,11 @@
+"use client";
+
 export function slugify(str: string): string {
   return str
+    .toString()
     .toLowerCase()
-    .replace(/\s+/g, "-")
-    .replace(/[^a-z0-9\-]/g, "");
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
 }
