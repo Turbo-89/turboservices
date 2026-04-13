48abdc563cffe5d80d47477aed7926d97167166d
Turbo-89 <verloo.wim@icloud.com>
2026-04-13 10:22:30 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `}
         </Script>
 
+        <Script id="gtm-base" strategy="afterInteractive">
+  {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WGHFPMJ3');
+  `}
+</Script>
+
+<noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-WGHFPMJ3"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  />
+</noscript>
 
         {/* Meta Pixel Code */}
         <Script id="fb-pixel" strategy="afterInteractive">
